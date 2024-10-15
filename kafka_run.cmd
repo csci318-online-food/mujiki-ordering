3f4c88ee2599271f78a8f7da4d202baf3237fd37
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2024-10-16 03:32:19 +1100

    fix: Fix Windows run scripts

--- kafka_run.cmd
+++ kafka_run.cmd
@@ -2,21 +2,27 @@
 set KAFKA_RELEASE=3.7.0
 set KAFKA_SCALA_VERSION=2.13
 
-cd %~dp0
+pushd %~dp0
 
 REM Check if Kafka exists
 if not exist ".kafka" (
     echo Downloading and installing Kafka
     mkdir ".kafka"
-    curl -SL https://archive.apache.org/dist/kafka/%KAFKA_RELEASE%/kafka_%KAFKA_SCALA_VERSION%-%KAFKA_RELEASE%.tgz | tar -xz -C ".kafka" --strip-components=1
+    curl -SL https://archive.apache.org/dist/kafka/%KAFKA_RELEASE%/kafka_%KAFKA_SCALA_VERSION%-%KAFKA_RELEASE%.tgz | tar -xzf - -C ".kafka" --strip-components=1
 )
 
 rmdir /s /q %~d0\tmp\zookeeper
 rmdir /s /q %~d0\tmp\kafka-logs
 rmdir /s /q .kafka\kafka-streams
 
-start cmd /c .kafka\bin\windows\zookeeper-server-start.bat .kafka\config\zookeeper.properties
+pushd .kafka
+
+start cmd /c bin\windows\zookeeper-server-start.bat config\zookeeper.properties
 
 timeout /t 1 /nobreak > NUL
 
-start cmd /c .kafka\bin\windows\kafka-server-start.bat .kafka\config\server.properties
+start cmd /c bin\windows\kafka-server-start.bat config\server.properties
+
+popd
+
+popd
